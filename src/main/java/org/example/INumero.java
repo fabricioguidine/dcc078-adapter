45026b12a92f1c591439f64ac809a019f4dedb97
fabricioguidine <fabricioguidine@gmail.com>
2023-09-24 21:36:01 -0300

    muda tipo de projeto

---
+++ src/main/java/org/example/INumero.java
@@ -0,0 +1,6 @@
+package org.example;
+
+public class INumero {
+    String getNumero();
+    void setNumero(String Numero);
+}
--- src/main/java/org/example/TemperaturaAdapter.java
+++
@@ -1,7 +0,0 @@
-package org.example;
-
-public class TemperaturaAdapter {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
-    }
-}
